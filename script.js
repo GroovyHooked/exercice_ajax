f428d73001b712f4977afb8c244ea16b059b44bf
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-03-15 23:28:32 +0100

    Work in progress

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ const results2 = document.querySelector(".results2");
 const openClose1 = document.querySelector(".first");
 const openClose2 = document.querySelector(".second");
 
-let arr = [];
-let artistsData = [];
 
-const stickerMaker = (element, result) => {
+const stickerMaker = ( data, resultDisplay) => {
   const div = document.createElement("div");
   div.className = "artist";
   const img = document.createElement("img");
   const h1 = document.createElement("h1");
-  img.src = element.image;
-  h1.innerText = element.name;
+  img.src = data.image;
+  h1.innerText = data.name;
   div.append(img, h1);
-  result.appendChild(div);
+  resultDisplay.appendChild(div);
 };
 
 const removeChilds = (parent) => {
@@ -25,47 +23,49 @@ const removeChilds = (parent) => {
   }
 };
 
-const storeInArray = (value) => {
-  value.forEach((element) => {
-    arr.push({ name: element.name, image: element.image });
-  });
+// const storeInArray = (value) => {
+//   value.forEach((element) => {
+//     arr.push({ name: element.name, image: element.image });
+//   });
+// };
+
+const functionFetch = (path) => {
+  fetch(path)
+    .then((response) => response.json())
+    .then((res) => stickerMaker({name: res.name, image: res.image}, results1))
+    .catch((err) => console.log(err))
 };
 
 const fetchArtist = () => {
-  fetch("./api/artists.json")
-    .then((response) => response.json())
-    .then((res) => storeInArray(res))
-    .catch((err) => console.log(err));
-  
   if (results1.hasChildNodes()) {
-    openClose1.innerHTML = "Closed";
+    openClose1.innerText = "Closed";
     removeChilds(results1);
   } else {
-    openClose1.innerHTML = "Open";
-    arr.forEach((element) => stickerMaker(element, results1));
+    openClose1.innerText = "Open";
+    functionFetch("./api/artists.json")
   }
-}
+};
 
 const asyncAwaitCall = async () => {
   const results = await fetch("./api/artists.json");
-  await results.json().then((e) => artistsData.push(e));
-  return artistsData[0].forEach((element) => stickerMaker(element, results2));
+  await results.json().then((e) => e.forEach((element) => {
+    stickerMaker({ name: element.name, image: element.image}, results2);
+  }));
 };
 
 const asyncArtist = () => {
-  //debugger
   if (results2.hasChildNodes()) {
-    openClose2.innerHTML = "Closed";
+    openClose2.innerText = "Closed";
     removeChilds(results2);
   } else {
-    openClose2.innerHTML = "Open";
+    openClose2.innerText = "Open";
     try {
       asyncAwaitCall();
     } catch (e) {
       console.error(e);
     }
   }
-}
+};
 
 promiseButton.addEventListener("click", fetchArtist);
 asyncAwaitButton.addEventListener("click", asyncArtist);
