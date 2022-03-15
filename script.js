974b19fdd161eb745b043fd29d60127f1208edbb
Thomas Cariot <dafrenchie2002@yahoo.fr>
2022-03-15 15:47:42 +0100

    Minor changes

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const asyncAwaitButton = document.querySelector("#async-await-button");
 const promiseButton = document.querySelector("#promise-button");
 const results1 = document.querySelector(".results1");
 const results2 = document.querySelector(".results2");
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+
 let arr = [];
 let artistsData = [];
 
@@ -27,28 +30,33 @@ const fetchData = async () => {
   results.json().then((e) => artistsData.push(e));
   return artistsData[0].forEach((element) => sticker(element, results2));
 };
+const storeInArray = (value) => {
+  value.forEach((element) => {
+    arr.push({ name: element.name, image: element.image });
+  });
+};
 
 promiseButton.addEventListener("click", () => {
   fetch("./api/artists.json")
     .then((response) => response.json())
-    .then((res) => {
-      res.forEach((element) => {
-        arr.push({ name: element.name, image: element.image });
-      });
-    })
+    .then((res) => storeInArray(res))
     .catch((err) => console.log(err));
 
   if (results1.hasChildNodes()) {
+    first.innerHTML = "Closed";
     removeChilds(results1);
   } else {
+    first.innerHTML = "Open";
     arr.forEach((element) => sticker(element, results1));
   }
 });
 
 asyncAwaitButton.addEventListener("click", () => {
   if (results2.hasChildNodes()) {
+    second.innerHTML = "Closed";
     removeChilds(results2);
   } else {
+    second.innerHTML = "Open";
     try {
       fetchData();
     } catch (e) {
